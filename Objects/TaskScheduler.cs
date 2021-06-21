10990edcd2eb0741fa5ea11f2ef22272eea2db70
Andrey Topchu <andreytopchu01@gmail.com>
2021-06-21 22:49:12 +0300

    Версия 1.1

---
+++ Objects/TaskScheduler.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Objects
+{
+    public class TaskScheduler:IJobExecutor
+    {
+        private Thread _startThread;
+        private Thread _stopThread;
+        private Thread _threadThatMakesTheMainThreadWait;
+
+        private volatile bool _isQueueProcessingComplete;
+        private volatile bool _isAllProcessingComplete;
+        private volatile bool _queueIsEmpty;
+
+        private ConcurrentQueue<Action> _queueActions;
+        private ConcurrentDictionary<Guid, Action> _runningTasks;
+
+        private const int StopDefaultTimeout = 3000;
+        private const int RefreshTimeout = 10;
+
+        public int Amount => _queueActions.Count;
+        public int RunningTasksCount => _runningTasks.Count;
+
+        public TaskScheduler()
+        {
+            _queueActions = new ConcurrentQueue<Action>();
+            _runningTasks = new ConcurrentDictionary<Guid, Action>();
+        }
+
+        public void Start(int maxConcurrent)
+        {
+            if (maxConcurrent <= 0 || maxConcurrent > 300)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxConcurrent));
+            }
+
+            InitializeStartThread(maxConcurrent);
+            InitializeThreadThatMakesTheMainThreadWait();
+
+            _startThread.Start();
+            _threadThatMakesTheMainThreadWait.Start();
+        }
+
+        public void Stop()
+        {
+            InitializeStopThread();
+            _stopThread.Start();
+        }
+
+        public void Add(Action action)
+        {
+            if (action == null) throw new ArgumentNullException();
+            _queueActions.Enqueue(action);
+            _queueIsEmpty = false;
+        }
+
+        public void Clear()
+        {
+            if(_queueIsEmpty) return;
+
+            while (_queueIsEmpty)
+            {
+                _queueActions.TryDequeue(out _);
+                if (Amount == 0) _queueIsEmpty = true;
+            }
+        }
+
+        public void LetTheSchedulerFinishCurrentSession()
+        {
+            _threadThatMakesTheMainThreadWait.Join();
+        }
+
+        private void InitializeStartThread(int maxConcurrent)
+        {
+            _startThread = new Thread(() =>
+            {
+                _isQueueProcessingComplete = false;
+
+                int freeSpace = maxConcurrent - RunningTasksCount;
+                SendingMaximumTasksForExecution(freeSpace);
+
+                while (!_isQueueProcessingComplete)
+                {
+                    freeSpace = WaitSomeMillisecondsAndGetFreeSpaceInRunningTasks(RefreshTimeout, 
+                        maxConcurrent);
+                    if (freeSpace > 0)
+                        SendingMaximumTasksForExecution(freeSpace);
+                }
+
+                if (_queueIsEmpty && RunningTasksCount == 0)
+                    _isAllProcessingComplete = true;
+            });
+        }
+
+        private void InitializeStopThread()
+        {
+            _stopThread = new Thread(() =>
+            {
+                _isQueueProcessingComplete = true;
+
+                while (RunningTasksCount != 0)
+                {
+                    Thread.Sleep(RefreshTimeout);
+                }
+
+                if(_queueIsEmpty) return;
+                Thread.Sleep(StopDefaultTimeout);
+                if (_isQueueProcessingComplete)
+                    _isAllProcessingComplete = true;
+            });
+        }
+
+        private void InitializeThreadThatMakesTheMainThreadWait()
+        {
+            _threadThatMakesTheMainThreadWait = new Thread(() =>
+            {
+                while (!_isAllProcessingComplete)
+                {
+                    if (_queueIsEmpty && RunningTasksCount == 0)
+                        _isAllProcessingComplete = true;
+
+                    Thread.Sleep(RefreshTimeout);
+                }
+            });
+        }
+
+        private void SendingMaximumTasksForExecution(int freeSpace)
+        {
+            if (Amount == 0)
+            {
+                _queueIsEmpty = true; return;
+            }
+
+            int count = Amount <= freeSpace ? Amount : freeSpace;
+            for (int i = 0; i < count; i++)
+            {
+                MoveNextActionToRunningTasksFromQueue();
+            }
+        }
+
+        private void MoveNextActionToRunningTasksFromQueue()
+        {
+            if (Amount == 0)
+            {
+                _queueIsEmpty = true; return;
+            }
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                var id = Guid.NewGuid();
+                _queueActions.TryDequeue(out var action);
+                _runningTasks[id] = action;
+
+                try
+                {
+                    action?.Invoke();
+                }
+                finally
+                {
+                    _runningTasks.TryRemove(id, out _);
+                }
+            });
+        }
+
+        private int WaitSomeMillisecondsAndGetFreeSpaceInRunningTasks(int millisecondsCount,
+            int maxConcurrent)
+        {
+            Thread.Sleep(millisecondsCount);
+            return maxConcurrent - RunningTasksCount;
+        }
+    }
+}
---
+++ Tests/TaskSchedulerTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects;
+
+namespace Tests
+{
+    [TestClass]
+    public class TaskSchedulerTest
+    {
+        public delegate void TaskForTaskScheduler();
+
+        public TaskScheduler GetTaskScheduler(int numberOfTasks, TaskForTaskScheduler task)
+        {
+            var taskScheduler = new TaskScheduler();
+
+            Action action = task.Invoke;
+
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                taskScheduler.Add(action);
+            }
+
+            return taskScheduler;
+        }
+
+        public void PrintRandomNumberAndSleepOneSecond()
+        {
+            var random = new Random();
+            var number = random.Next(999);
+            Console.WriteLine(number);
+            Thread.Sleep(1000);
+        }
+
+        [TestMethod]
+        public void TaskSchedulerSuccessWorkTest()
+        {
+            var taskScheduler = GetTaskScheduler(35, PrintRandomNumberAndSleepOneSecond);
+
+            var threadsCount = 4;
+            taskScheduler.Start(threadsCount);
+
+            taskScheduler.LetTheSchedulerFinishCurrentSession();
+            Assert.AreEqual(0, taskScheduler.RunningTasksCount);
+        }
+
+        [TestMethod]
+        public void SuccessfulParallelOperationOfTheTaskSchedulerTest()
+        {
+            var timer = new Stopwatch();
+            var taskScheduler = GetTaskScheduler(8, PrintRandomNumberAndSleepOneSecond);
+
+            var threadsCount = 4;
+
+            timer.Start();
+            taskScheduler.Start(threadsCount);
+            taskScheduler.LetTheSchedulerFinishCurrentSession();
+            timer.Stop();
+
+            Console.WriteLine("Задачи выполнены за " + timer.ElapsedMilliseconds);
+
+            Assert.AreEqual(0, taskScheduler.RunningTasksCount);
+            Assert.AreEqual(0, taskScheduler.Amount);
+            Assert.IsTrue(timer.ElapsedMilliseconds<=3000);
+        }
+
+        [TestMethod]
+        public void StoppingWorkTaskSchedulerWhenTheQueueIsNotEmptyTest()
+        {
+            var taskScheduler = GetTaskScheduler(16, PrintRandomNumberAndSleepOneSecond);
+
+            var threadsCount = 3;
+            
+            taskScheduler.Start(threadsCount);
+            Thread.Sleep(3000);
+
+            taskScheduler.Stop();
+            taskScheduler.LetTheSchedulerFinishCurrentSession();
+
+            Assert.AreEqual(0, taskScheduler.RunningTasksCount);
+            Assert.AreEqual(7, taskScheduler.Amount);
+        }
+
+        [TestMethod]
+        public void AddingTasksInTaskSchedulerWhenItWorksTest()
+        {
+            var taskScheduler = GetTaskScheduler(16, PrintRandomNumberAndSleepOneSecond);
+
+            var threadsCount = 3;
+
+            taskScheduler.Start(threadsCount);
+
+            Thread.Sleep(2000);
+            var amount = taskScheduler.Amount;
+
+            taskScheduler.Add(PrintRandomNumberAndSleepOneSecond);
+
+            Assert.AreEqual(amount + 1, taskScheduler.Amount);
+
+            taskScheduler.LetTheSchedulerFinishCurrentSession();
+
+            Assert.AreEqual(0, taskScheduler.RunningTasksCount);
+        }
+    }
+}
